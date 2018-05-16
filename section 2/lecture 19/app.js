ab6321db39cbaa37ae0006ea59c8193ad9ec57be
Jay Velasco <jayvelasco1990>
2018-05-15 23:20:16 -0400

    lecture 19

---
+++ section 2/lecture 19/app.js
@@ -0,0 +1,13 @@
+const EventEmitter = require('events')
+
+const emitter = new EventEmitter()
+
+emitter.on('messageLogged', function(){
+	console.log('listener called')
+})
+
+emitter.emit('messageLogged')
+
+
+
+//making a noise - produce something
