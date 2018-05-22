dcfdd23b982ca4fd0844e377c4480566f5fcdd67
Jay Velasco <jayvelasco1990>
2018-05-21 22:58:47 -0400

    node course

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,10 @@
+const Joi = require('joi')
+
 const express = require('express')
 
 const app = express()
+
+app.use(express.json())
 
 const courses = [
 	{ id: 1, name: 'course1' },
@@ -16,11 +20,65 @@ app.get('/api/courses', (req, res) => {
 	res.send(courses)
 })
 
+app.post('/api/courses', (req, res) => {
+
+	const { error } = validateCourse(req.body)
+
+	if(error) return res.status(400).send(error.details[0].message)
+
+	const course = {
+		id: courses.length + 1,
+		name: req.body.name
+	}
+
+	courses.push(course)
+
+	res.send(course)
+})
+
 // /api/courses/1
 app.get('/api/courses/:id', (req, res) => {
 	const course = courses.find(c => c.id === parseInt(req.params.id))
 
-	if(!course) res.status(404).send('The course with the given ID was not found')
+	if(!course) return res.status(404).send('The course with the given ID was not found')
+
+	res.send(course)
+})
+
+app.put('/api/courses/:id', (req, res) => {
+	
+	const course = courses.find(c => c.id === parseInt(req.params.id))
+
+	if(!course) return res.status(404).send('The course with the given ID was not found')
+
+	const { error } = validateCourse(req.body)
+
+	if(error) return res.status(400).send(error.details[0].message)
+
+	course.name = req.body.name
+
+	res.send(course)
+})
+
+function validateCourse(course){
+
+	const schema = {
+		name: Joi.string().min(3).required()
+	}
+
+	const result = Joi.validate(course, schema)
+
+	return result
+}
+
+app.delete('/api/courses/:id', (req, res) =>{
+	const course = courses.find(c => c.id === parseInt(req.params.id))
+
+	if(!course) return res.status(404).send('The course with the given ID was not found')
+
+	var index = courses.indexOf(course)
+
+	courses.splice(index, 1)
 
 	res.send(course)
 })
@@ -31,3 +89,4 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
 	console.log(`listening on port ${port}...`)
 })
+
