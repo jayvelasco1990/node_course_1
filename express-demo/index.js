1ca686540f860909379ec2819113bdf161eb7b13
Jay Velasco <jayvelasco1990>
2018-05-26 18:38:26 -0400

    restructuring app

--- express-demo/index.js
+++ express-demo/index.js
@@ -8,9 +8,13 @@ const helmet = require('helmet')
 
 const Joi = require('joi')
 
-const logger = require('./logger')
+const logger = require('./middleware/logger')
 
 const authenticator = require('./authenticator')
+
+const courses = require('./routes/courses')
+
+const home = require('./routes/home')
 
 const express = require('express')
 
@@ -25,6 +29,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true })) //key=value&key=value (req.body)
 
 app.use(express.static('public'))
+
+app.use('/api/courses', courses)
+
+app.use('/', home)
 
 app.use(helmet())
 
@@ -44,82 +52,6 @@ app.use(logger)
 
 app.use(authenticator)
 
-const courses = [
-	{ id: 1, name: 'course1' },
-	{ id: 2, name: 'course2' },
-	{ id: 3, name: 'course3' },
-]
-
-app.get('/', (req, res)=> {
-	res.render('index', { title: 'My Express App', message: 'Hello'})
-})
-
-app.get('/api/courses', (req, res) => {
-	res.send(courses)
-})
-
-app.post('/api/courses', (req, res) => {
-
-	const { error } = validateCourse(req.body)
-
-	if(error) return res.status(400).send(error.details[0].message)
-
-	const course = {
-		id: courses.length + 1,
-		name: req.body.name
-	}
-
-	courses.push(course)
-
-	res.send(course)
-})
-
-// /api/courses/1
-app.get('/api/courses/:id', (req, res) => {
-	const course = courses.find(c => c.id === parseInt(req.params.id))
-
-	if(!course) return res.status(404).send('The course with the given ID was not found')
-
-	res.send(course)
-})
-
-app.put('/api/courses/:id', (req, res) => {
-	
-	const course = courses.find(c => c.id === parseInt(req.params.id))
-
-	if(!course) return res.status(404).send('The course with the given ID was not found')
-
-	const { error } = validateCourse(req.body)
-
-	if(error) return res.status(400).send(error.details[0].message)
-
-	course.name = req.body.name
-
-	res.send(course)
-})
-
-function validateCourse(course){
-
-	const schema = {
-		name: Joi.string().min(3).required()
-	}
-
-	const result = Joi.validate(course, schema)
-
-	return result
-}
-
-app.delete('/api/courses/:id', (req, res) =>{
-	const course = courses.find(c => c.id === parseInt(req.params.id))
-
-	if(!course) return res.status(404).send('The course with the given ID was not found')
-
-	var index = courses.indexOf(course)
-
-	courses.splice(index, 1)
-
-	res.send(course)
-})
 
 //port
 const port = process.env.PORT || 3000
