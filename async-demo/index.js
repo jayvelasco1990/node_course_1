8728b940a945f723a88d050230db8e8491ec4c06
Jay Velasco <jayvelasco1990>
2018-05-27 20:55:12 -0400

    promise

---
+++ async-demo/index.js
@@ -0,0 +1,32 @@
+console.log('Before')
+
+getUser(1, getRepositories)
+
+console.log('After')
+
+function getRepositories(user) {
+	getRepositories(user.gitHubUsername, getCommits)
+}
+
+function getCommits(repos) {
+	getCommits(repo, displayCommits)
+}
+
+function displayCommits(commits) {
+	console.log(commits)
+}
+
+function getUser(id, callback) {
+	setTimeout(() => {
+		console.log('Reading a user from a database...')
+		callback({ id: id, gitHubUsername: 'mosh' })
+	}, 2000)
+}
+
+function getRepositories(username, callback) {
+	console.log('get repos', callback)
+	setTimeout(() => {
+		console.log('Calling GitHub API...')
+		callback(['repo1', 'repo2', 'repo3'])
+	}, 2000)
+}
