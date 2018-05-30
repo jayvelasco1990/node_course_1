075521f4b6ede3916cdc926fb86f0d426a3d441b
Jay Velasco <jayvelasco1990>
2018-05-29 23:14:03 -0400

    async

--- async-demo/index.js
+++ async-demo/index.js
@@ -8,11 +8,31 @@ console.log('Before')
 // 	})
 // })
 
-getUser(1)
-	.then(user => getRepositories(user.gitHubUsername))
-	.then(repos => getCommits(repos[0]))
-	.then(commits => console.log('Commits', commits))
-	.catch(err => console.log('Error', err.message))
+// getUser(1)
+// 	.then(user => getRepositories(user.gitHubUsername))
+// 	.then(repos => getCommits(repos[0]))
+// 	.then(commits => console.log('Commits', commits))
+// 	.catch(err => console.log('Error', err.message))
+
+
+//async and await
+
+async function displayCommits(){
+	try{
+		const user = await getUser(1)
+
+		const repos = await getRepositories(user.gitHubUsername)
+
+		const commits = await getCommits(repos[0])
+
+		console.log(commits)
+	}
+	catch(err){
+		console.log('Error', err.message)
+	}
+}
+
+displayCommits()
 
 console.log('After')
 
@@ -30,7 +50,8 @@ function getRepositories(username) {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
 			console.log('Calling GitHub API...')
-			resolve(['repo1', 'repo2', 'repo3'])
+			// resolve(['repo1', 'repo2', 'repo3'])
+			reject(new Error('Could not get the repos.'))
 		}, 2000)
 	})
 }
