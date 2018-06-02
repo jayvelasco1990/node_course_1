2ec8284bcf5db08dd0f4f5c739acc225d076f120
Jay Velasco <jayvelasco1990>
2018-06-01 23:43:31 -0400

    mongoose

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,3 +3,33 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/playground')
 	.then(()=>console.log('Connected to MongoDB...'))
 	.catch(err => console.log('Could not connect to MongoDB...', err))
+
+const courseSchema = new mongoose.Schema({
+	name: String,
+	author: String,
+	tags: [String],
+	date: { type: Date, default: Date.now },
+	isPublished: Boolean
+})
+
+// Classes, objects
+// Course, nodeCourse
+
+const Course = mongoose.model('Course', courseSchema)
+
+async function createCourse() {
+
+	const course = new Course({
+		name: 'Angular Course',
+		author: 'Mosh',
+		tags: ['angular', 'frontend'],
+		isPublished: true
+	})
+
+	const result = await course.save()
+
+	console.log(result)
+}
+
+
+createCourse() 
