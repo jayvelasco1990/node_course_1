fa004e7f9e95afe43ecdafeba885d428c14ce162
Jay Velasco <jayvelasco1990>
2018-06-02 07:45:59 -0400

    mongo

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -31,5 +31,31 @@ async function createCourse() {
 	console.log(result)
 }
 
+async function getCourses() {
+	//eq (equal)
+	//ne (not equal)
+	//gt (greater than)
+	//gte (greater than or equal to)
+	//lt (less than)
+	//lte (less than or equal to)
+	//in
+	//nin (not in)
+	// .find({price: { $gte: 10, $lte: 20 } })
+	// .find({ price: { $in: [10, 15, 20] } })
+	//or
+	//and
 
-createCourse() 
+	const courses = await Course
+		// .find({ author: 'Mosh', isPublished: true})
+		.find()
+		.or([{ author: 'Mosh'}, { isPublished: true } ])
+		.and([])
+		.limit(10)
+		.sort({ name: 1})
+		.select({ name: 1, tags: 1 })
+
+	console.log(courses)
+}
+
+
+getCourses() 
