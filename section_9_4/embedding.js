90f0fafa51bab20acce8265dbd399fd087c2c33c
Jay Velasco <jayvelasco1990>
2018-06-17 21:38:08 -0400

    section 9

--- section_9_4/embedding.js
+++ section_9_4/embedding.js
@@ -13,13 +13,14 @@ const authorSchema = new mongoose.Schema({
 const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
-  name: String
+  name: String,
+  authors: [authorSchema]
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name, 
-    author
+    authors
   }); 
   
   const result = await course.save();
@@ -31,4 +32,34 @@ async function listCourses() {
   console.log(courses);
 }
 
-createCourse('Node Course', new Author({ name: 'Mosh' }));
+async function addAuthor(courseId, author) {
+  const course = await Course.findById(courseId)
+  course.authors.push(author)
+  course.save()
+}
+
+async function updateAuthor(courseId) {
+  const course = await Course.update( { _id: courseId }, {
+    $unset: {
+      'author': ''
+    }
+  })
+
+}
+
+async function removeAuthor(courseId, authorId){
+  const course = await Course.findById(courseId)
+  const author = course.authors.id(authorId)
+  author.remove()
+  course.save()
+}
+
+removeAuthor('5b270b56cd12346821d1f42d', '5b270c270cc9156c6a75d65f')
+
+// addAuthor('5b270b56cd12346821d1f42d', new Author({name: 'Amy'}))
+
+// createCourse('Node Course', [
+//   new Author({ name: 'Mosh' }),
+//   new Author({ name: 'John' })
+// ]);
+// updateAuthor('5b2708359f70b857ae37fe5d')
