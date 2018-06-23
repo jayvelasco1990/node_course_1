a9d0c24cfe96b024b790b868d45886aaee8f0579
Jay Velasco <jayvelasco1990>
2018-06-23 19:33:54 -0400

    finished project

--- vidly/routes/rentals.js
+++ vidly/routes/rentals.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const { Rental, validate } = require('../models/rental')
 
 router.get('/', async (req, res) => {
+
 	const rentals = await Rental.find()
 	.populate('genre')
 	.populate('customer')
@@ -14,6 +15,7 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
+
 	const { error } = validate(req.body)
 
 	if (error) return res.status(404).send(error.details[0].message + ' bad model')
@@ -37,6 +39,43 @@ router.post('/', async (req, res) => {
 	catch(ex) {
 		for (field in ex.errors) ex.errors[field].message
 	}
+})
+
+router.put('/:id', async (req, res) => {
+
+	const rental = await Rental.findByIdAndUpdate(req.params.id, {
+		$set: {
+			movie: req.body.movie,
+			genre: req.body.genre,
+			customer: req.body.customer,
+			checkoutDate: req.body.checkoutDate,
+			returnDate: req.body.returnDate,
+			rentalFee: req.body.rentalFee
+		}
+	}, { new: true })
+
+	if (!rental) return res.status(404).send('rental was not found')
+
+	res.send(rental)
+})
+
+router.delete('/:id', async (req, res) => {
+
+	const rental = await Rental.findByIdAndRemove(req.params.id)
+
+	if (!rental) return res.status(404).send('rental was not found')
+
+	res.send(rental)
+})
+
+router.get('/:id', async (req, res) => {
+	
+	const rental = await Rental.findById(req.params.id)
+
+	if (!rental) return res.status(404).send('rental does not exist')
+
+	res.send(rental)
+
 })
 
 module.exports = router
