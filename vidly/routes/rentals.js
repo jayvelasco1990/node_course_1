3fd06217ada6021f9ce0f8c1b54ad297d03963e3
Jay Velasco <jayvelasco1990>
2018-06-23 18:47:16 -0400

    post

--- vidly/routes/rentals.js
+++ vidly/routes/rentals.js
@@ -13,4 +13,30 @@ router.get('/', async (req, res) => {
 	res.send(rentals)
 })
 
+router.post('/', async (req, res) => {
+	const { error } = validate(req.body)
+
+	if (error) return res.status(404).send(error.details[0].message + ' bad model')
+
+	try{
+		let rental = new Rental({
+			movie: req.body.movie,
+			genre: req.body.genre,
+			customer: req.body.customer,
+			checkoutDate: req.body.checkoutDate,
+			returnDate: req.body.returnDate,
+			rentalFee: req.body.rentalFee
+		})
+
+		rental = await rental.save()
+
+		if (!rental) return res.status(404).send('failed to save rental')
+
+		res.send(rental)
+	}
+	catch(ex) {
+		for (field in ex.errors) ex.errors[field].message
+	}
+})
+
 module.exports = router
