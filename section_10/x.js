68d0a955ec701c82b2fb209ae7bb5400116d3ba8
Jay Velasco <jayvelasco1990>
2018-06-27 22:30:32 -0400

    section 10

---
+++ section_10/x.js
@@ -0,0 +1,17 @@
+// /api/genres
+// /api/movies
+// /api/customers
+// /api/rentals
+
+// Authentication 
+// Authorization 
+
+// Register: POST /api/users { name, email, password }
+// Login: POST /api/logins
+
+//implement api to register new users
+
+email: {
+	type: String,
+	unique: true
+}
