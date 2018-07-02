aca32737c14e1a34cf53f24175f4fe51797ecfcc
Jay Velasco <jayvelasco1990>
2018-07-01 20:44:55 -0400

    user api

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -37,4 +37,43 @@ router.post('/', async (req, res) => {
 	}
 })
 
+router.put('/:id', async (req, res) => {
+
+	const { error } = validate(req.body)
+
+	if (error) return res.status(404).send(error.details[0].message)
+
+	const user = await User.findByIdAndUpdate(req.params.id, {
+		$set: {
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password
+		}
+	}, { new : true})
+
+	if (!user) return res.status(404).send('User does not exist')
+
+	res.send(user)
+
+})
+
+router.delete('/:id', async (req, res) => {
+
+	const user = await User.findByIdAndRemove(req.params.id)
+
+	if (!user) return res.status(404).send('User does not exist')
+
+	res.send(user)
+
+})
+
+router.get('/:id', async (req, res) => {
+	const user = await User.findById(req.params.id)
+
+	if (!user) return res.status(404).send('User does not exist')
+
+	res.send(user)
+
+})
+
 module.exports = router
