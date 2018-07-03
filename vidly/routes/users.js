f3d7c12584e25a8887f84220b86d7c76b270a1eb
Jay Velasco <jayvelasco1990>
2018-07-02 22:25:35 -0400

    api

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const express = require('express')
 
 const router = express.Router()
@@ -15,18 +17,19 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
 	const { error } = validate(req.body)
 
-	if (error) return res.status(404).send(error.details[0].message)
+	if (error) return res.status(400).send(error.details[0].message)
 
 	try {
-		const user = new User({
-			name: req.body.name,
-			email: req.body.email,
-			password: req.body.password
-		})
+
+		const existingUser = await User.findOne({ email: req.body.email })
+
+		if (existingUser) return res.status(400).send('user aleady exists')
+
+		const user = new User(_.pick(req.body, [ 'name', 'email', 'password' ]))
 
 		await user.save()
 
-		res.send(user)
+		res.send(_.pick(user, [ '_id', 'name', 'email' ]))
 	}
 	catch (ex) {
 		for (field in ex.errors){
