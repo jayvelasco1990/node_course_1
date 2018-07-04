a8e703e80b474d740e668b00e2b21ca8ca0a41e7
Jay Velasco <jayvelasco1990>
2018-07-03 22:47:38 -0400

    bcrypt

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -4,6 +4,8 @@ const express = require('express')
 
 const router = express.Router()
 
+const bcrypt = require('bcrypt')
+
 const { User, validate } = require('../models/user')
 
 router.get('/', async (req, res) => {
@@ -26,6 +28,10 @@ router.post('/', async (req, res) => {
 		if (existingUser) return res.status(400).send('user aleady exists')
 
 		const user = new User(_.pick(req.body, [ 'name', 'email', 'password' ]))
+
+		const salt = await bcrypt.genSalt(10)
+
+		user.password = await bcrypt.hash(user.password, salt)
 
 		await user.save()
 
