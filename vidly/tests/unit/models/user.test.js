219222c6f17dcd72dff5f506cf57df7ba0437408
Jay Velasco <jayvelasco1990>
2018-07-25 21:09:16 -0400

    section 13

---
+++ vidly/tests/unit/models/user.test.js
@@ -0,0 +1,26 @@
+const { User } = require('../../../models/user')
+
+const jwt = require('jsonwebtoken')
+
+const config = require('config')
+
+const mongoose = require('mongoose')
+
+//https://stackoverflow.com/questions/19760829/how-to-set-id-to-db-document-in-mongoose
+describe('user.generateAuthToken', () => {
+	it('should return a valid JWT', () => {
+		
+		const payload = { 
+			_id: new mongoose.Types.ObjectId().toHexString(), 
+			isAdmin: true 
+		}
+
+		const user = new User(payload)
+		
+		const token = user.generateAuthToken()
+
+		const decoded = jwt.verify(token, config.get('jwtPrivateKey'))
+
+		expect(decoded).toMatchObject(payload)
+	})
+})
